b4453a61a7650986fdb964343fff60f9b09a47c8
Ernesto <ernestomtez@gmail.com>
2021-09-19 12:46:47 +0200

    tarea_45

---
+++ tarea_45/busqueda.py
@@ -0,0 +1,38 @@
+from funciones_busqueda import f_nit_sec, f_nit_bin
+
+#Se crea un lista de elementos que se solicitarán al usuario.
+l_data = []
+while True:
+    try:
+        n = int(input("Introduce un elemento de la lista. Si quieres cerrar la lista, introduce cualquier carácter no numérico: "))
+        l_data.append(n)
+    except ValueError:
+        if len(l_data) == 0:
+            print("La lista de elemento está vacía, por lo que volvemos a empezar.")
+            True
+        else:
+            print("La lista de elementos a analizar es:" + str(l_data))
+            break
+
+#Se solicita al usuario cuál es el elemento que se desea buscar en la lista
+n = int(input("¿Cuál es el elemento que quieres buscar en: " + str(l_data) + "? "))
+while True:
+    try:
+        if n not in l_data:
+            True
+            n = int(input("El elemento no se encuentra en la lista. Introduzca un elemento que se encuentre en el arreglo: "))
+        else:
+            break
+    except:
+        n = int(input("El elemento no es válido. Introduzca un elemento que se encuentre en la lista: "))
+        True
+
+
+
+#Se llama a las funciones de búsqueda:
+print("El número de iteraciones con el algorítmo de búsqueda secuencial es: " + str(f_nit_sec(n, l_data)))
+print("El número de iteraciones con el algorítmo de búsqueda binario es: " + str(f_nit_bin(n, l_data)))
+
+
+
+
---
+++ tarea_45/funciones_busqueda.py
@@ -0,0 +1,31 @@
+#Se contabiliza en número de iteraciones mediante búsqueda secuencial. 
+#Si el elemento se encuentra varias veces en la lista, se selecciona el primero.
+def f_nit_sec(p_int, p_lista):
+    n_int_sec = 1
+    for i in p_lista:
+        if i != p_int:
+            n_int_sec = n_int_sec + 1
+        else:
+            break
+    return(n_int_sec)
+
+#Se contabiliza en número de iteraciones mediante búsqueda binaria.
+def f_nit_bin(p_int, p_lista):
+    n_int_bin = 1
+    p_lista.sort()
+    print("La lista ordenada es: " + str(p_lista))
+    #print(len(p_lista))
+    #print(p_lista[int(len(p_lista)/2)])
+    middle_index = int(len(p_lista)/2)
+    l_newlist = p_lista
+    while p_int != l_newlist[middle_index]:
+        n_int_bin = n_int_bin + 1
+        if p_int > l_newlist[middle_index]:
+            l_newlist = l_newlist[middle_index:]
+        else:
+            l_newlist = l_newlist[:middle_index]
+        
+        middle_index = int(len(l_newlist)/2)
+        #print(l_newlist)
+            
+    return(n_int_bin)
